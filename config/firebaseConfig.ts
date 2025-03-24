e53ae75f77856dbc961b4c5ff67c355572afae61
gabbykarry <feranmikarry@gmail.com>
2025-03-24 18:42:30 +0100

    ...

--- config/firebaseConfig.ts
+++ config/firebaseConfig.ts
@@ -2,38 +2,28 @@ import { initializeApp } from "firebase/app";
 import {
   getAuth,
   initializeAuth,
-  browserSessionPersistence,
   // @ts-ignore
-  getReactNativePersistence
+  getReactNativePersistence,
 } from "firebase/auth";
 
 import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
-import { Platform } from "react-native";
-
 
 // Firebase configuration object (from Firebase Console)
 const firebaseConfig = {
-  apiKey: process.env.EXPO_PUBLIC_API_KEY,
-  authDomain: process.env.EXPO_PUBLIC_AUTH_DOMAIN,
-  projectId: process.env.EXPO_PUBLIC_PROJECT_ID,
-  storageBucket: process.env.EXPO_PUBLIC_STORAGE_BUCKET,
-  messagingSenderId: process.env.EXPO_PUBLIC_MESSAGING_SENDER_ID,
-  appId: process.env.EXPO_PUBLIC_APP_ID,
+  apiKey: "AIzaSyBhuPx-EZwTsguTnEGXuGkSzGy9GcOWwl0",
+  authDomain: "nile-app-e98a8.firebaseapp.com",
+  projectId: "nile-app-e98a8",
+  storageBucket: "nile-app-e98a8.firebasestorage.app",
+  messagingSenderId: "1025056029039",
+  appId: "1:1025056029039:web:56331e780c0d79e335509a",
 };
-
-console.log("API KEY:", process.env.EXPO_PUBLIC_API_KEY);
-
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// const persistence = Platform.OS === 'web'
-//            ? browserSessionPersistence
-//            : getReactNativePersistence(ReactNativeAsyncStorage);
-
 // Initialize Auth with persistence
 const auth = initializeAuth(app, {
-  persistence: getReactNativePersistence(ReactNativeAsyncStorage)
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage),
 });
 
 // Export auth instance
